9cc00ee3b8370bb9c8a9d35474a1545014207feb
Hadis Sulejmani <hadissulejmani@outlook.com>
2022-09-04 22:38:40 +0200

    minor changes

--- Aerodrom/Form1.vb
+++ Aerodrom/Form1.vb
@@ -1,7 +1,7 @@
 ﻿Imports System
 Imports System.Windows.Forms
 Imports System.Data.SqlClient
-
+Imports Aerodrom.WINFORMS_DBDataSetTableAdapters
 
 Public Class Form1
 
@@ -12,8 +12,8 @@ Public Class Form1
         InitializeComponent()
         Dim airport As Airport = New Airport("Александар Велики", "Скопје", "SKP")
         Dim destination As Destination = New Destination("Виена", 1240, 260)
-        ListBox1.Items.Add(airport.toString)
-        'loadDestination()
+        lbAirports.Items.Add(airport.toString)
+        loadDestination()
     End Sub
 
 
@@ -23,30 +23,20 @@ Public Class Form1
 
         If newAirport.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
             'ListBox1.Items.Add(NewAirport.TextBox3.Text & " - " & NewAirport.TextBox2.Text & " - " & NewAirport.TextBox1.Text)
-            ListBox1.Items.Add(newAirport.Airport)
+            lbAirports.Items.Add(newAirport.Airport)
         End If
-    End Sub
-
-    Private Sub ListBox1_SelectedIndexChanged(ByVal sender As Object, ByVal e As EventArgs) Handles ListBox1.SelectedIndexChanged
-        Dim airport As Airport
-        For Each airport In ListBox1.Items
-            ListBox1.DisplayMember = airport.toString()
-        Next
-        ' btnDeleteDestination.Enabled = lbAirports.SelectedIndex <> -1
-        'btnNewDestination.Enabled = lbAirports.SelectedIndex <> -1
-        ' loadDestination()
     End Sub
 
     Private Sub btnAddDestination_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Button3.Click
         'Dim form As NewDestination = New Destination()
         Dim newDestination As NewDestination = New NewDestination()
         If newDestination.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
-            ListBox2.Items.Add(newDestination.Destination.toString)
+            lbDestinations.Items.Add(newDestination.Destination.toString)
         End If
         'If (Form.ShowDialog() = Windows.Forms.DialogResult.OK) Then
         '' Dim airport As Airport = TryCast(lbAirports.SelectedItem, Airport)
         ' airport.NewDestination(Form.Destination)
-        '   loadDestination()
+        ' loadDestination()
         'End If
 
 
@@ -58,26 +48,46 @@ Public Class Form1
     End Sub
 
     'Private Sub loadDestination()
-    '  lbDestinations.Items.Clear()
-    ' tbAverage.Clear()
-    ' tbLongest.Clear()
-    'Dim airport As Airport = TryCast(airport.SelectedItem, Airport)
-    ' If airport IsNot Nothing AndAlso airport.Destinations.Count > 0 Then
-    ' Dim maxPrice As Destination = airport.Destinations(0)
-    'Dim totalDistance As Single = 0
-    ' For Each destination As Destination In airport.Destinations
-    '   destination.Items.Add(destination)
-    '   If destination.Price > maxPrice.Price Then
-    '      maxPrice = destination
-    '  End If
-    '  totalDistance += destination.Length
-    ' Next
-    ' tbAverage.Text = String.Format("{0:#.0}", totalDistance / lbDestinations.Items.Count)
-    '  tbLongest.Text = maxPrice.toString()
-    'End If
+    '    lbDestinations.Items.Clear()
+    '    tbAverage.Dispose()
+    '    tbMaxPrice.Dispose()
+    '    Dim airport As Airport = TryCast(lbAirports.SelectedItem, Airport)
+
+    '    If airport IsNot Nothing AndAlso airport.Destinations.Count > 0 Then
+    '        Dim maxPrice As Destination = airport.Destinations(0)
+    '        Dim totalDistance As Single = 0
+    '        For Each destination As Destination In airport.Destinations
+    '            destination.Items.Add(destination)
+    '            If destination.price > maxPrice.price Then
+    '                maxPrice = destination
+    '            End If
+    '            totalDistance += destination.length
+    '        Next
+    '        tbAverage.Text = String.Format("{0:#.0}", totalDistance / lbDestinations.Items.Count)
+    '        tbMaxPrice.Text = maxPrice.toString()
+    '    End If
 
     'End Sub
 
+    Private Sub loadDestination()
+        lbDestinations.Items.Clear()
+        tbAverage.Dispose()
+        tbMaxPrice.Dispose()
+        Dim maxPrice As Integer = 0
+        Dim totalDistance As Integer = 0
+
+        If lbDestinations.Items.Count > 0 Then
+            maxPrice = lbDestinations.Items.Item(0)
+            For Each destination As Destination In lbDestinations.Items
+                If destination.getPrice() > maxPrice Then
+                    maxPrice = destination.getPrice()
+                End If
+                totalDistance += destination.getLength()
+            Next
+            tbAverage.Text = String.Format("{0:#.0}", totalDistance / lbDestinations.Items.Count)
+            tbMaxPrice.Text = maxPrice.ToString()
+        End If
+    End Sub
 
     'Private Sub ListBox1_SelectedIndexChanged(ByVal sender As Object, ByVal e As EventArgs) Handles ListBox1.SelectedIndexChanged
     '    Dim airport As Airport
@@ -96,7 +106,7 @@ Public Class Form1
     Private Sub btnRemoveAirport_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Button2.Click
         Dim DialogResult As DialogResult = MessageBox.Show("Дали сте сигурни за да го избришете аеродромот !?", "Remove airport", MessageBoxButtons.YesNo)
         If (DialogResult = DialogResult.Yes) Then
-            ListBox1.Items.Remove(ListBox1.SelectedItem)
+            lbAirports.Items.Remove(lbAirports.SelectedItem)
         ElseIf (DialogResult = DialogResult.No) Then
             'do something else
         End If
@@ -116,15 +126,32 @@ Public Class Form1
         'DataGridView1.DataSource = ds.Tables(0)
     End Sub
 
+    Private Sub tbMaxPrice_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tbMaxPrice.Click
+        loadDestination()
+    End Sub
 
-
-    Private Sub Najskapa_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Najskapa.Click
-
+    Private Sub tbAverage_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tbAverage.Click
 
     End Sub
 
-    Private Sub Prosek_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Prosek.Click
+    Private Sub lbAirports_SelectedIndexChanged(ByVal sender As Object, ByVal e As EventArgs) Handles lbAirports.SelectedIndexChanged
+        Dim airport As Airport
+        For Each airport In lbAirports.Items
+            lbAirports.DisplayMember = airport.toString()
+        Next
+        ' btnDeleteDestination.Enabled = lbAirports.SelectedIndex <> -1
+        'btnNewDestination.Enabled = lbAirports.SelectedIndex <> -1
+        loadDestination()
+    End Sub
 
+    Private Sub lbDestinations_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lbDestinations.SelectedIndexChanged
+        Dim destination As Destination
+        For Each destination In lbDestinations.Items
+            lbDestinations.DisplayMember = destination.toString()
+        Next
+        ' btnDeleteDestination.Enabled = lbAirports.SelectedIndex <> -1
+        ' btnNewDestination.Enabled = lbAirports.SelectedIndex <> -1
+        ' loadDestination()
     End Sub
 End Class
 
